1b529c9645c13e1fe5405c935e08ae42d546a727
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2025-11-13 13:31:15 -0500

    feat: complete about page with shared sections and responsive layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav__toggle--btn');
 const mainNav = document.querySelector('.main__nav');
 const overlay = document.querySelector('.overlay');
+const quoteToggleBtns = document.querySelectorAll('.director__btn');
+const quote = document.querySelector('.director__card');
 
 const toggleMenu = () => {
   const isExpanded = navToggle.getAttribute('aria-expanded') === 'true';
@@ -13,6 +15,15 @@ const toggleMenu = () => {
   console.log(isExpanded);
 };
 
+const toggleQuote = (e) => {
+  const button = e.currentTarget;
+  const isExpanded = button.getAttribute('aria-expanded') === 'true';
+
+  button.setAttribute('aria-expanded', !isExpanded);
+
+  quote.classList.toggle('show');
+};
+
 navToggle.addEventListener('click', toggleMenu);
 
 overlay.addEventListener('click', toggleMenu);
@@ -21,4 +32,14 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && mainNav.classList.contains('open')) {
     toggleMenu();
   }
+});
+
+quoteToggleBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    const isExpanded = button.getAttribute('aria-expanded') === 'true';
+
+    button.setAttribute('aria-expanded', !isExpanded);
+    const card = button.closest('.director__card');
+    card.classList.toggle('show');
+  });
 });
